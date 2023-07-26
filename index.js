8d3b5818d0a684c3d81e5ce0b6314aed9e9d42ab
darinetag <tagdarinefatma@gmail.com>
2023-07-26 21:39:23 +0100

    completed assignment

--- index.js
+++ index.js
@@ -1,11 +1,33 @@
-function scuberGreetingForFeet(){
-  // Write your code here!
+function scuberGreetingForFeet(distance) {
+  if (distance <= 400) {
+  return "This one is on me!";
+  } else if (distance > 2500) {
+    return "No can do." 
+  } else {
+    "I will gladly take your thirty bucks."
+}
+if (distance <= 2000){
+  return "That will be twenty bucks."
+}
+if (distance >= 2000){
+  return "I will gladly take your thirty bucks."
+} 
+}
+function ternaryCheckCity(city){
+  if (city === "NYC") {
+    return "Ok, sounds good."
+  } else {
+    return "No go."
+  }
 }
 
-function ternaryCheckCity(){
-  // Write your code here!
-}
-
-function switchOnCharmFromTip(){
-  // Write your code here!
+function switchOnCharmFromTip(tip){
+  switch (tip) {
+  case "generous" : 
+  return "Thank you so much."
+    case "not as generous" :
+      return "Thank you." 
+    default: 
+    return"Bye."
+  }
 }
